b81391898abc8a5f5d64117e042c8861900c35de
Santiago <pelaezas007@gmail.com>
2023-10-12 18:27:39 -0500

    Base course finished

---
+++ src/app/dbz/components/list/list.component.ts
@@ -0,0 +1,18 @@
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Character } from '../../interfaces/character.interface';
+
+@Component({
+  selector: 'dbz-list',
+  templateUrl: './list.component.html',
+  styleUrls: ['./list.component.css']
+})
+export class ListComponent {
+
+  @Output()
+  public onDeleteCharacter: EventEmitter<string> = new EventEmitter();
+
+  @Input()
+  public characters: Character[] = [];
+
+  deleteCharacter = (id: string): void => this.onDeleteCharacter.emit(id);
+}
